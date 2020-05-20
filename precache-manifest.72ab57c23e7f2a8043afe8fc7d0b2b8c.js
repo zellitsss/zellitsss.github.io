0012d84a6b1de97889208adbe4dd5399bba108ca
zellitsss <zellitsss@gmail.com>
2020-05-20 17:54:14 +0700

    Updates

---
+++ precache-manifest.72ab57c23e7f2a8043afe8fc7d0b2b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be31b424b3948e54c3a591325596ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72355d5f54b75ba6eb6a",
+    "url": "/static/css/main.78e9cf31.chunk.css"
+  },
+  {
+    "revision": "bc99956a5287b787599d",
+    "url": "/static/js/2.5184fc19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5184fc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72355d5f54b75ba6eb6a",
+    "url": "/static/js/main.d5bba786.chunk.js"
+  },
+  {
+    "revision": "e2947287b093c9cd693f",
+    "url": "/static/js/runtime-main.f64bbb04.js"
+  }
+]);
